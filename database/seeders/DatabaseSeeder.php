0fe4dc248e7f57fcb277a082442de50aeab5fe21
Louise <lacielo@gbox.ncf.edu.ph>
2026-05-01 16:50:36 +0800

    Local commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            InventorySeeder::class,
+            // InventorySeeder::class,
         ]);
     }
 }
